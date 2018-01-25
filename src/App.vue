ac6d67a4f611b76e0a225c82525c0661a016dcf8
Shawn Rider <shawn@shawnrider.com>
2018-01-25 13:50:41 -0800

    Initial commit.
    Change-Id: I47656766ddd74492c7c963e88422a50f318c26a8

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      class="purple"
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+
+    <v-footer fixed app class="purple">
+      <span>AdvanceOS v0.1</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'AdvanceOS Employee Interface'
+    }
+  },
+  name: 'App'
+}
+</script>
